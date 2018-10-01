1559e54d45836d79bd8539c8122d46bca014fcaa
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-01 07:34:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object( {artist: 'song'} ) ;
+var playlist = new Object( {artistName: 'songTitle'} ) ;
 
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artist]: song})
